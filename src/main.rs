8e201dd368f5155e2737409a44aa675c6ac3c716
Olivier Leduc <leduc.o@gmail.com>
2023-10-13 13:56:19 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::net::SocketAddr;
+
+use axum::{
+    routing::{get, post},
+    Router,
+};
+
+async fn echo(body: String) -> &'static str {
+    println!("{body}");
+    return "ok";
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", post(echo)).route("/", get(echo));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
